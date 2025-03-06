37e25cb6683af3c975ab427bb1d4b2b1277e6b36
Abdullah_Saif <126707579+AbdullahaSaif@users.noreply.github.com>
2025-03-06 15:26:31 +0500

    Fizz_Buzz.py

--- Semester_3/AI_Lab/Lab-2/Mini-Project-1/Fizz_Buzz.py
+++ Semester_3/AI_Lab/Lab-2/Mini-Project-1/Fizz_Buzz.py
@@ -11,34 +11,42 @@
 # The player has to answer as per the rules of the game.
 
 import random
+
 def fizz_buzz_game():
-    number = int(input("Enter the number: "))
+    number = int(input("Enter the range of numbers: "))
+    max_range = int(input("Enter the maximum range for random number: "))
     score = 0
-    for i in random.sample(range(1, number + 1), number):
+    last_number = 0
+
+    for i in range(number):
+        random_number = random.randint(0, max_range)
+        i = last_number + random_number
+        print(f"Random number: {random_number}")
         print(f"Number: {i}")
+        last_number = i
         print("Choose the correct option:")
         print("1. Fizz")
         print("2. Buzz")
         print("3. Fizz Buzz")
         print("4. N/A")
         answer = int(input("Your choice: "))
-        
-        answers = ["Fizz", "Buzz", "Fizz Buzz", "N/A"] # Answers list
-        
+
+        answers = ["Fizz", "Buzz", "Fizz Buzz", "N/A"]  # Answers list
+
         if i % 3 == 0 and i % 5 == 0:  # Fizz Buzz
             correct_answer = 3
-        elif i % 3 == 0: # Fizz
+        elif i % 3 == 0:  # Fizz
             correct_answer = 1
-        elif i % 5 == 0: # Buzz
+        elif i % 5 == 0:  # Buzz
             correct_answer = 2
-        else: # fizzbuzz
+        else:  # N/A
             correct_answer = 4
 
-        if answer == correct_answer: # Correct answer
+        if answer == correct_answer:  # Correct answer
             print("Correct!")
             score += 1
         else:
-            print(f"Wrong! Correct answer is {answers[correct_answer - 1]}") # Show correct answer from answers list with -1 index for correct answer
+            print(f"Wrong! Correct answer is {answers[correct_answer - 1]}")  # Show correct answer from answers list with -1 index for correct answer
             break
 
     print(f"Total score is: {score}")
