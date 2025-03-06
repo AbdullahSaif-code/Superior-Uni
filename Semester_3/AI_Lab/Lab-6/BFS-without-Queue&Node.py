63b8c8adb0dc98e138848285ee5ed98dbe48de70
Abdullah_Saif <126707579+AbdullahaSaif@users.noreply.github.com>
2025-03-06 15:48:00 +0500

    BFS-without-Queue&Node.py

--- Semester_3/AI_Lab/Lab-6/BFS-without-Queue&Node.py
+++ Semester_3/AI_Lab/Lab-6/BFS-without-Queue&Node.py
@@ -7,20 +7,26 @@ graph = {
     'F': ['H']
 }
 
-visited = []
+def bfs(graph, start, goal):
+    visited = []
+    current_level = [start]
 
-def bfs_recursive(graph, node, goal):
-    visited.append(node)
-    print(node, end=" ")
+    while current_level:
+        next_level = []
+        for node in current_level:
+            if node not in visited:
+                visited.append(node)
+                print(node, end=" ")
 
-    if node == goal:
-        print(f"\nGoal node {goal} found!")
-        return True
+                if node == goal:
+                    print(f"\nGoal node {goal} found!")
+                    return True
 
-    for neighbour in graph.get(node, []):
-        if neighbour not in visited:
-            if bfs_recursive(graph, neighbour, goal):
-                return True
+                next_level.extend(graph.get(node, []))
+        current_level = next_level
+
+    print("\nGoal node not found.")
+    return False
 
 print("BFS Search:")
-bfs_recursive(graph, 'A', 'E')
+bfs(graph, 'A', 'E')
